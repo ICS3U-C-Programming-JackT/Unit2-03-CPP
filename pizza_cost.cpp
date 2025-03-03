8c51f57c3ec7149a6b9f1cb39a7c729472c103b8
Jack Turcotte <jack.turcotte@stu.ocsb.ca>
2025-03-03 23:48:21 +0000

    Created pizza_cost.cpp

---
+++ pizza_cost.cpp
@@ -0,0 +1,30 @@
+// Copyright (c) 2025 Jack Turcotte All rights reserved.
+//
+// Created by: Jack Turcotte
+// Date: March 3, 2025
+// This program determines the cost of a pizza
+// based on user input
+
+//import all libraries
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+
+const float HST = 0.13;
+const float LABOUR_COST = 2.00;
+const float RENTAL_COST = 2.25;
+const float INGREDIENT_COST = 1.50;
+
+int main() {
+    float diameter;
+
+    std::cout << "Enter the diameter of your pizza (inches): ";
+    std::cin >> diameter;
+
+    float subtotal = LABOUR_COST + RENTAL_COST + INGREDIENT_COST * diameter;
+    float tax = subtotal * HST;
+    float total = tax + subtotal;
+
+    std::cout << std::fixed << std::setprecision(2);
+    std::cout << "The total cost of your pizza will be: $" << total << std::endl;
+}
